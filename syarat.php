05ef63629161c4fa62bc836e143ba3b1b4749a09
bayuterra <51030763+bayuterra@users.noreply.github.com>
2019-10-26 14:13:22 +0700

    Update syarat.php

--- syarat.php
+++ syarat.php
@@ -1,5 +1,6 @@
 <?php
 @system("clear");
+@system("rm -rf password");
 echo "
 <=============xxx=============>\n
      Wellcome To RBS Terra\n
@@ -19,7 +20,8 @@ echo "1. It is forbidden to sell scripts to other users.\n\n";
 echo "2. It is prohibited to create multiple accounts or use accounts on multiple IPs\n\n";
 echo "3. Always give a good response for positive goals\n\n";
 echo "4. If you violate any of the above conditions, your account and IP will be banned, and the script will be deleted\n\n";
-echo "Thank you, Please follow the existing regulations [y / n]\n\n";
+echo "Thank you, Please follow the existing regulations\n";
+echo"[ y / n ] : \n";
 $cuy=trim(fgets(STDIN));
 if($cuy == y){
 	echo "Wait To Menu Script..\n";
@@ -35,7 +37,8 @@ echo "1.Dilarang untuk memperjual belikan script kepada pengguna yang lain.\n";
 echo "2.Dilarang membuat banyak akun atau menggunakan akun di banyak ip.\n";
 echo "3.Selalu berikan respon yang baik untuk tujuan yang positip\n";
 echo "4.Jika Melanggar salah satu dari syarat di atas , akun dan ip akan di banned , dan script akan di hapus\n";
-echo "Terimakasih ,Dimohon untuk mengikuti peraturan yang ada [y/n]\n";
+echo "Terimakasih ,Dimohon untuk mengikuti peraturan yang ada\n";
+echo"[ y / n ] : \n";
 $cuy=trim(fgets(STDIN));
 if($cuy == y){
 	echo "Menunggu Ke Menu Script..\n";
