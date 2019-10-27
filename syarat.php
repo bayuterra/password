44fd614ff2fac8a6655f4932a2ad60946d9afd17
bayuterra <51030763+bayuterra@users.noreply.github.com>
2019-10-27 23:54:50 +0700

    Update syarat.php

--- syarat.php
+++ syarat.php
@@ -5,8 +5,8 @@ echo "
 <=============xxx=============>\n
      Wellcome To RBS Terra\n
 <======[Assalamu-Alaikum]======>\n";
-echo "please choose your language\nsilahkan pilih bahasa anda";
-echo "en/id :";
+echo "please choose your language\nsilahkan pilih bahasa anda\n";
+echo "[ en / id ]\n";
 $bhs=trim(fgets(STDIN));
 if ($bhs == "en"){
 echo "terms and conditions for RBS script users.\n";
@@ -14,9 +14,10 @@ echo "1. It is forbidden to sell scripts to other users.\n";
 echo "2. It is prohibited to create multiple accounts or use accounts on multiple IPs\n";
 echo "3. Always give a good response for positive goals\n";
 echo "4. If you violate any of the above conditions, your account and IP will be banned, and the script will be deleted\n";
-echo "Thank you, Please follow the existing regulations [y / n]\n";
+echo "Thank you, Please follow the existing regulations \n[ y / n ]\n";
 $cuy=trim(fgets(STDIN));
 }else{
+	echo "Bye Bye\n";
 	exit();
 }
 if ($bhs == "id"){
@@ -25,16 +26,17 @@ echo "1.Dilarang untuk memperjual belikan script kepada pengguna yang lain.\n";
 echo "2.Dilarang membuat banyak akun atau menggunakan akun di banyak ip.\n";
 echo "3.Selalu berikan respon yang baik untuk tujuan yang positip\n";
 echo "4.Jika Melanggar salah satu dari syarat di atas , akun dan ip akan di banned , dan script akan di hapus\n";
-echo "Terimakasih ,Dimohon untuk mengikuti peraturan yang ada [y/n]\n";
+echo "Terimakasih ,Dimohon untuk mengikuti peraturan yang ada \n[ y / n ]\n";
 $cuy=trim(fgets(STDIN));
 }else{
+	echo "Bye Bye\n";
 	exit();
 }
 if($cuy == y){
 	echo "Menunggu Ke Menu Script..\n";
 	sleep(3);
 	}else{
-		echo "Selamat Tinggal\n";
+		echo "Bye Bye\n";
 		exit();
 		}
 ?>
