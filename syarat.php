f664795360d52c2717063e97ffaf713b9fa10467
bayuterra <51030763+bayuterra@users.noreply.github.com>
2019-10-28 00:07:24 +0700

    Update syarat.php

--- syarat.php
+++ syarat.php
@@ -8,34 +8,40 @@ echo "
 echo "please choose your language\nsilahkan pilih bahasa anda\n";
 echo "[ en / id ]\n";
 $bhs=trim(fgets(STDIN));
+@system("clear");
+echo "
+<=============xxx=============>\n
+     Wellcome To RBS Terra\n
+<======[Assalamu-Alaikum]======>\n";
+echo "please choose your language\nsilahkan pilih bahasa anda\n";
 if ($bhs == "en"){
 echo "\nterms and conditions for RBS script users.\n";
 sleep(1.5);
-echo "1. It is forbidden to sell scripts to other users.\n";
+echo "\n1. It is forbidden to sell scripts to other users.\n";
 sleep(1.5);
-echo "2. It is prohibited to create multiple accounts or use accounts on multiple IPs\n";
+echo "\n2. It is prohibited to create multiple accounts or use accounts on multiple IPs\n";
 sleep(1.5);
-echo "3. Always give a good response for positive goals\n";
+echo "\n3. Always give a good response for positive goals\n";
 sleep(1.5);
-echo "4. If you violate any of the above conditions, your account and IP will be banned, and the script will be deleted\n";
+echo "\n4. If you violate any of the above conditions, your account and IP will be banned, and the script will be deleted\n";
 sleep(1.5);
-echo "5. Prohibited Encode, Decode, Decomply, This script without permission, Please W.A for business purposes\n";
+echo "\n5. Prohibited Encode, Decode, Decomply, This script without permission, Please W.A for business purposes\n";
 sleep(1.5);
-echo "Thank you, Please follow the existing regulations \n[ y / n ]\n";
+echo "\nThank you, Please follow the existing regulations \n[ y / n ]\n";
 $cuy=trim(fgets(STDIN));
 }
 if ($bhs == "id"){
 echo "\nsyarat dan ketentuan untuk pengguna script RBS.\n";
 sleep(1.5);
-echo "1.Dilarang untuk memperjual belikan script kepada pengguna yang lain.\n";
+echo "\n1.Dilarang untuk memperjual belikan script kepada pengguna yang lain.\n";
 sleep(1.5);
-echo "2.Dilarang membuat banyak akun atau menggunakan akun di banyak ip.\n";
+echo "\n2.Dilarang membuat banyak akun atau menggunakan akun di banyak ip.\n";
 sleep(1.5);
-echo "3.Selalu berikan respon yang baik untuk tujuan yang positip\n";
+echo "\n3.Selalu berikan respon yang baik untuk tujuan yang positip\n";
 sleep(1.5);
-echo "4.Jika Melanggar salah satu dari syarat di atas , akun dan ip akan di banned , dan script akan di hapus\n";
+echo "\n4.Jika Melanggar salah satu dari syarat di atas , akun dan ip akan di banned , dan script akan di hapus\n";
 sleep(1.5);
-echo "5.Dilarang Encode , Decode , Decomply, Script ini tanpa izin , Silahkan W.A untuk keperluan bisnis\n";
+echo "\n5.Dilarang Encode , Decode , Decomply, Script ini tanpa izin , Silahkan W.A untuk keperluan bisnis\n";
 sleep(1.5);
 echo "Terimakasih ,Dimohon untuk mengikuti peraturan yang ada \n[ y / n ]\n";
 $cuy=trim(fgets(STDIN));
